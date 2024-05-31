729bcc045c7b3cff743f3248cf38b794e3205636
amr <amrmasri999@gmail.com>
2024-05-31 11:52:30 +0300

    gfjio

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -1,6 +1,20 @@
 import { useState, useEffect } from "react"
 
 function DigitalClock (){
+
+    const [time , setTime] = useState(new Date ())
+
+    useEffect(() => {
+            const intervalId = setinterval(() => {
+                    setTime (new Date)
+            }, 1000);
+
+            return () => {
+                clearInterval(intervalId);
+            }
+     } , [])
+
+
     return(
         <div className="clock-container">
             <div className="clock">
