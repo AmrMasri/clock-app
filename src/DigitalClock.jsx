560569fd8c81c2ecccd9919d6d65399ff261a621
amr <amrmasri999@gmail.com>
2024-05-31 12:23:06 +0300

    dvsfs

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -5,23 +5,37 @@ function DigitalClock (){
     const [time , setTime] = useState(new Date ())
 
     useEffect(() => {
-            const intervalId = setinterval(() => {
-                    setTime (new Date)
+            const intervalId = setInterval(() => {
+                    setTime (new Date());
             }, 1000);
 
-            return () => {
+            return() => {
                 clearInterval(intervalId);
             }
-     } , [])
+        }, [] )  
 
+        function formatTime(){
+            let hours = time.getHours();
+            const minutes = time.getMinutes();
+            const seconds = time.getSeconds();
+            const meridiem = hours >= 12 ? "PM" : "AM" 
+
+            hours= hours % 12 || 12
+
+            return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}${meridiem}`
+        }
+
+        function padZero(number){
+            return (number < 10 ? "0" : "") + number
+        }
 
     return(
         <div className="clock-container">
             <div className="clock">
-                <span>00:00:00</span>
+                <span>{formatTime()}</span>
             </div>
         </div>
     );
 }
 
-export default DigitalClock
+export default DigitalClock;
